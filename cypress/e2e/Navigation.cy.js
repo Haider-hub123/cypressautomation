55b1bdaee21e290ccdda82839fffcf7f872dd19c
Haider-hub123 <haider.ali@azm.dev>
2025-07-06 21:01:44 +0500

    for slack notification

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -1,6 +1,6 @@
 describe('my suite', function() {
 
-  it('NaavigationTest', function() {
+  it('Navigation test 123', function() {
     cy.visit('https://automationteststore.com/');
     cy.title().should('eq', 'A place to practice your automation skills!');
 
